3b539bc9ff8180690b92f4549aca8ef67495ae9e
Samira Barrera <164458958+samirabarrera@users.noreply.github.com>
2024-10-26 00:48:14 -0600

    first commit

---
+++ Segunda sección/ejercicio2.js
@@ -0,0 +1,32 @@
+//Ejercicio 2: Motor de Recomendación de Productos
+//Diseñe una función para recomendar productos a un usuario basado en varios factores.
+
+const usuario1 = {
+    age: 22,
+    isMember: true,
+    purchaseHistory: () => {
+        tech = 6,
+        fashion = 1,
+        other = 2
+    }
+
+}
+const usuario2 = {
+    age: {min: 25, max: 40},
+    isMember: false,
+    purchaseHistory: {
+        tech: 1,
+        fashion: 1,
+        other: 1
+    }
+}
+const usuarios = (usuario1, usuario2) => {
+        if (age === 22 && isMember === true) {
+            return ("High-Tech Product")
+        } else {
+            return ("Low-Tech Product")
+        }
+    } 
+    function recommendProduct (age, isMember, purchaseHistory) {
+        console.log(recommendProduct(22, true, { tech: 6, fashion: 1, other: 2 }));
+    }
